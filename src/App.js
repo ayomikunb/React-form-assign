401657008a31d5bb0be79ebc9e333b77394b9198
ayomikunb <ayo.bakare199@gmail.com>
2022-10-20 22:37:56 +0100

    my-react-form

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
+import React, {Component} from 'react'
 import './App.css';
+import {Button, Form, FormGroup, Label, Input} from 'reactstrap';
+import { InstagramLoginButton} from 'react-social-login-buttons';
 
-function App() {
+class App extends Component {
+  render(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+    <Form className="login-form">
+      <h1 className='text-center'>LOGIN PAGE</h1>
+      <h2 className='text-center'>Welcome...</h2>
+      <FormGroup>
+        <Label>Email</Label>
+        <Input type='email' placeholder='Email'/>
+      </FormGroup>
+      <FormGroup>
+        <Label>Password</Label>
+        <Input type='password' placeholder='Password'/>
+      </FormGroup>
+      <div className="d-grid gap-2">
+      <Button variant="secondary" size="lg">
+        Log In
+      </Button>
     </div>
+      <div className='text-center pt-3'>
+        Or continue with your social account
+      </div>
+      <InstagramLoginButton className='mt-3 mb-3'/>
+    <div className='text-center'>
+        <a href='/sign-up'>Sign up</a>
+        <span className='p-2'>|</span>
+        <a href='/sign-up'>Forgot Password</a>
+    </div>
+       </Form>
+       </div>
   );
+  }
 }
 
 export default App;
